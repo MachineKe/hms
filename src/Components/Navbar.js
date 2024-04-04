417190112053216ae3a90ba8ba7d6e4f670fb86c
machineke <markarapsoi72@gmail.com>
2024-04-04 12:48:02 +0300

    working on auth

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,30 +1,76 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
-const user = JSON.parse(localStorage.getItem('currentUser'))
+  const user = JSON.parse(localStorage.getItem("currentUser"));
+  function logout() {
+    localStorage.removeItem('currentUser')
+    window.location.href = '/login'
+  }
   return (
     <div>
-<nav class="navbar navbar-expand-lg">
-  <a class="navbar-brand" href="#">5🌟HOTEL</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-              <a class="nav-link" href="/register">
-                Register
+      <nav className="navbar navbar-expand-lg">
+        <a className="navbar-brand" href="#">
+          5🌟HOTEL
         </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/login">Login</a>
-      </li>
-        </ul>
-  </div>
-</nav>
-    </div>
-  )
-}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" ><i class="fa fa-hamburger" style={{color:'white'}}></i></span>
+        </button>
 
-export default Navbar
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr5">
+            {user ? (
+              <li className="nav-item dropdown">
+                <div className="dropdown">
+                  <button
+                    className="btn btn-secondary dropdown-toggle"
+                    type="button"
+                    id="dropdownMenuButton"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    <i className="fa fa-user"></i>{user.name}
+                  </button>
+                  <div
+                    className="dropdown-menu"
+                    aria-labelledby="dropdownMenuButton"
+                  >
+                    <a className="dropdown-item" href="#">
+                      Bookings
+                    </a>
+                    <a className="dropdown-item" href="#" onClick={logout}>
+                      Logout
+                    </a>
+                  </div>
+                </div>
+              </li>
+            ) : (
+              <>
+                <li className="nav-item active">
+                  <a className="nav-link" href="/register">
+                    Register
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/login">
+                    Login
+                  </a>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
